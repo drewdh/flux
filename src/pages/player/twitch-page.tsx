3822bf8db26426483c69015301a1e7a23144b7fa
Drew Hanberry <drew.hanberry1@gmail.com>
2024-04-01 15:14:50 -0700

    refactor: change variable name for design tokens

--- src/pages/player/twitch-page.tsx
+++ src/pages/player/twitch-page.tsx
@@ -2,6 +2,8 @@ import DhAppLayout from 'common/flux-app-layout';
 import TwitchComponent from './twitch';
 import { useEffect, useRef, useState } from 'react';
 
+const viewportBreakpointXs = 688;
+
 export default function TwitchPage() {
   const ref = useRef<HTMLDivElement>(null);
   const [disableContentPaddings, setDisableContentPaddings] = useState<boolean>(false);
@@ -13,7 +15,7 @@ export default function TwitchPage() {
     }
     const resizeObserver = new ResizeObserver((entries) => {
       const { width } = entries[0].contentRect;
-      setDisableContentPaddings(width < 688);
+      setDisableContentPaddings(width < viewportBreakpointXs);
     });
     resizeObserver.observe(refCurrent);
     return () => resizeObserver.unobserve(refCurrent);

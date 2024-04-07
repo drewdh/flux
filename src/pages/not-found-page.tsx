1d6de9f18df43aea78d6d58bdb7a8a9b93a4b5a1
Drew Hanberry <drew.hanberry1@gmail.com>
2024-04-06 19:12:41 -0700

    feat(not-found-page): restyle

--- src/pages/not-found-page.tsx
+++ src/pages/not-found-page.tsx
@@ -1,17 +1,42 @@
-import Alert from '@cloudscape-design/components/alert';
+import Box from '@cloudscape-design/components/box';
+import { useEffect, useState } from 'react';
 
 import FluxAppLayout from 'common/flux-app-layout';
 import useTitle from 'utilities/use-title';
+import { topNavId } from '../top-navigation/constants';
 
 export default function NotFoundPage() {
   useTitle('Page not found - Flux');
+  const [navHeight, setNavHeight] = useState<number>(0);
+
+  useEffect(() => {
+    const navObserver = new ResizeObserver((entries) => {
+      setNavHeight(entries[0].contentRect.height);
+    });
+    navObserver.observe(document.getElementById(topNavId)!);
+    return () => navObserver.disconnect();
+  }, [navHeight]);
 
   return (
     <FluxAppLayout
       maxContentWidth={350}
       toolsHide
+      disableContentPaddings
       navigationHide
-      content={<Alert>The page you are looking for does not exist.</Alert>}
+      content={
+        <div
+          style={{
+            display: 'flex',
+            justifyContent: 'center',
+            alignItems: 'center',
+            height: `calc(100vh - ${navHeight}px)`,
+          }}
+        >
+          <Box fontWeight="bold" fontSize="heading-xl">
+            This page does not exist.
+          </Box>
+        </div>
+      }
     />
   );
 }

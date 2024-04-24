f3d01988a2e3c906347ad378642e7bb3a16b74ab
Drew Hanberry <drew.hanberry1@gmail.com>
2024-04-24 15:12:01 -0700

    feat: add more space to media in container

--- src/pages/player/game-details.tsx
+++ src/pages/player/game-details.tsx
@@ -22,31 +22,40 @@ export default function GameDetails({ gameId }: GameDetailsProps) {
     return null;
   }
 
-  const imgSrc = data.data[0].box_art_url.replace('{width}x{height}', '144x200');
+  const imgSrc = data.data[0].box_art_url.replace('{width}x{height}', '240x320');
   const href = interpolatePathname(Pathname.Game, { gameId });
 
   return (
     <div className={styles.gameDetailsWrapper}>
       <InternalLink href={href}>
-        <Container
-          media={{
-            content: <img src={imgSrc} height="104" width="72" />,
-            position: 'side',
-            width: 72,
-          }}
-        >
-          <Box fontSize="heading-xs" fontWeight="bold" padding={{ bottom: 'xs' }}>
-            {data.data[0].name}
-          </Box>
-          {igdbData && (
-            <Box fontSize="body-s" color="text-body-secondary">
-              <SpaceBetween size="xxs" direction="horizontal">
-                {format(igdbData?.[0].first_release_date * 1000, 'yyyy')}
-                &bull;
-                {igdbData?.[0].genres[0].name}
-              </SpaceBetween>
+        <Container disableContentPaddings>
+          <div style={{ display: 'flex' }}>
+            <div style={{ margin: '8px 0 8px 8px', borderRadius: '8px', overflow: 'hidden' }}>
+              <img
+                style={{
+                  display: 'block',
+                }}
+                src={imgSrc}
+                height="160"
+                width="120"
+                alt={data.data[0].name}
+              />
+            </div>
+            <Box padding="l">
+              <Box fontSize="heading-s" fontWeight="bold" padding={{ bottom: 'xs' }}>
+                {data.data[0].name}
+              </Box>
+              {igdbData && (
+                <Box color="text-body-secondary">
+                  <SpaceBetween size="xxs" direction="horizontal">
+                    {format(igdbData?.[0].first_release_date * 1000, 'yyyy')}
+                    &bull;
+                    {igdbData?.[0].genres[0].name}
+                  </SpaceBetween>
+                </Box>
+              )}
             </Box>
-          )}
+          </div>
         </Container>
       </InternalLink>
     </div>

82bc8783025a9d4f7b598240d04e01a155eb3d77
Drew Hanberry <drew.hanberry1@gmail.com>
2024-06-24 10:38:19 -0700

    chore: remove getuser call from chat message

--- src/pages/player/chat-message.tsx
+++ src/pages/player/chat-message.tsx
@@ -1,16 +1,11 @@
-import Box from '@cloudscape-design/components/box';
-import Icon from '@cloudscape-design/components/icon';
-import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
-import { faBadgeCheck } from '@fortawesome/pro-solid-svg-icons';
+import { ReactNode } from 'react';
 import Link from '@cloudscape-design/components/link';
+import clsx from 'clsx';
 
 import styles from './chat.module.scss';
 import Avatar from 'common/avatar';
-import { useGetUsers } from '../../api/api';
 import Emote from './emote';
-import { ReactNode } from 'react';
 import { ChatEvent } from '../../api/twitch-types';
-import clsx from 'clsx';
 
 export function Message({ message }: Props) {
   return (
@@ -52,11 +47,6 @@ export function Message({ message }: Props) {
 }
 
 export default function ChatMessage({ message, onClick, variant = 'normal' }: Props) {
-  const { data: userData } = useGetUsers({
-    ids: [message.chatter_user_id, message.reply?.parent_user_id ?? ''].filter(Boolean),
-  });
-  const user = userData?.data[0];
-
   return (
     <div
       onClick={onClick}
@@ -65,7 +55,6 @@ export default function ChatMessage({ message, onClick, variant = 'normal' }: Pr
       {message.reply && variant === 'normal' && (
         <div className={styles.replyWrapper}>
           <div className={styles.replyConnector} />
-          <Avatar userId={message.reply.parent_user_id} size="xxs" />
           <div className={styles.replyTextWrapper}>
             @<span>{message.reply.parent_user_name}</span> {message.reply.parent_message_body}
           </div>
@@ -74,28 +63,22 @@ export default function ChatMessage({ message, onClick, variant = 'normal' }: Pr
       <div className={styles.messageWrapper}>
         <div className={variant === 'featured' ? styles.newReplyConnectorWrapper : styles.collapse}>
           <div className={variant === 'featured' ? styles.newReplyConnector : styles.collapse} />
-          <Avatar userId={message.chatter_user_id} size="s" />
+          {variant === 'featured' && <Avatar userId={message.chatter_user_id} size="s" />}
         </div>
         <div>
           {variant === 'normal' && (
             <span className={styles.username}>
-              {/*<b style={{ color: message.color }}>{message.chatter_user_name}</b>*/}
-              <b>{message.chatter_user_name}</b>
-              {user?.broadcaster_type === 'partner' && (
-                <Box variant="span" padding={{ left: 'xxs' }}>
-                  <Icon svg={<FontAwesomeIcon icon={faBadgeCheck} color="#a970ff" />} />
-                </Box>
-              )}
+              <b style={{ color: message.color }}>{message.chatter_user_name}</b>
+              {/*{user?.broadcaster_type === 'partner' && (*/}
+              {/*  <Box variant="span" padding={{ left: 'xxs' }}>*/}
+              {/*    <Icon svg={<FontAwesomeIcon icon={faBadgeCheck} color="#a970ff" />} />*/}
+              {/*  </Box>*/}
+              {/*)}*/}
             </span>
           )}
           <Message message={message} variant={variant} />
         </div>
       </div>
-      {/*{variant === 'featured' && (*/}
-      {/*  <div className={styles.newReplyConnectorWrapper}>*/}
-      {/*    <div className={styles.newReplyConnector} />*/}
-      {/*  </div>*/}
-      {/*)}*/}
     </div>
   );
 }

13bc7c640527324daa53cd596a8867a2fbf03d93
Drew Hanberry <drew.hanberry1@gmail.com>
2024-07-19 12:25:52 -0700

    chore: migrate category results to use flux cards

--- src/pages/results/category-results.tsx
+++ src/pages/results/category-results.tsx
@@ -1,18 +1,14 @@
 import { useEffect, useState } from 'react';
-import Cards, { CardsProps } from '@cloudscape-design/components/cards';
 import { PaginationProps } from '@cloudscape-design/components/pagination';
-import Header from '@cloudscape-design/components/header';
 import { NonCancelableCustomEvent } from '@cloudscape-design/components';
 
 import { useSearchCategories } from '../../api/api';
-import useCounter from 'utilities/use-counter';
 import { CategoryResult } from '../../api/twitch-types';
-import InternalLink from 'common/internal-link';
 import { interpolatePathname, Pathname } from 'utilities/routes';
 import Empty from 'common/empty/empty';
-import FluxImage from 'common/flux-image';
-import styles from './styles.module.scss';
 import InfinitePagination from 'common/infinite-pagination';
+import FluxCards from 'common/cards';
+import CategoryThumbnail from 'common/category-thumbnail';
 
 export default function CategoryResults({ query }: Props) {
   const [currentPageIndex, setCurrentPageIndex] = useState<number>(1);
@@ -27,68 +23,33 @@ export default function CategoryResults({ query }: Props) {
       pageSize: 12,
     });
 
-  const counter = useCounter({
-    count: data?.pages.flatMap((page) => page.data).length ?? 0,
-    isOpenEnd: hasNextPage,
-    isLoading: isLoading,
-  });
-
   const items = data?.pages[currentPageIndex - 1]?.data ?? [];
 
   function handlePaginationChange(event: NonCancelableCustomEvent<PaginationProps.ChangeDetail>) {
     setCurrentPageIndex(event.detail.currentPageIndex);
   }
 
-  const cardDefinition: CardsProps.CardDefinition<CategoryResult> = {
-    header: (item) => {
-      const href = interpolatePathname(Pathname.Game, { gameId: item.id });
-      return (
-        <InternalLink href={href} fontSize="heading-m">
-          {item.name}
-        </InternalLink>
-      );
-    },
-    sections: [
-      {
-        id: 'image',
-        content: (item) => {
-          const href = interpolatePathname(Pathname.Game, { gameId: item.id });
-          const imgSrc = item?.box_art_url.replace(/-\d+x\d+/g, `-${400}x${534}`);
-          return (
-            <InternalLink href={href}>
-              <FluxImage className={styles.categoryBoxArt} src={imgSrc} alt={item.name} />
-            </InternalLink>
-          );
-        },
-      },
-    ],
-  };
-
   return (
-    <Cards
-      stickyHeader
+    <FluxCards<CategoryResult>
+      columns={5}
       empty={<Empty header="No matches" />}
-      header={
-        <Header
-          actions={
-            <InfinitePagination
-              data={data}
-              currentPageIndex={currentPageIndex}
-              onChange={handlePaginationChange}
-              fetchNextPage={fetchNextPage}
-              isFetchingNextPage={isFetchingNextPage}
-              hasNextPage={hasNextPage}
-            />
-          }
-          counter={counter}
-        >
-          Categories
-        </Header>
-      }
+      itemMapper={(item) => {
+        const href = interpolatePathname(Pathname.Game, { gameId: item.id });
+        const imgSrc = item?.box_art_url.replace(/-\d+x\d+/g, `-${400}x${534}`);
+        return <CategoryThumbnail imgSrc={imgSrc} href={href} title={item.name} />;
+      }}
       items={items}
-      cardDefinition={cardDefinition}
-      loadingText="Loading categories"
       loading={isFetching}
+      pagination={
+        <InfinitePagination
+          data={data}
+          currentPageIndex={currentPageIndex}
+          onChange={handlePaginationChange}
+          fetchNextPage={fetchNextPage}
+          isFetchingNextPage={isFetchingNextPage}
+          hasNextPage={hasNextPage}
+        />
+      }
     />
   );
 }

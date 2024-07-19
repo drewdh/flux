c9ff4840883cc64c4f9c2976e3b5b494a2737ccb
Drew Hanberry <drew.hanberry1@gmail.com>
2024-07-19 12:25:37 -0700

    chore: create category thumbnail component

---
+++ src/common/category-thumbnail/index.tsx
@@ -0,0 +1,29 @@
+import Box from '@cloudscape-design/components/box';
+
+import InternalLink from 'common/internal-link';
+import FluxImage from 'common/flux-image';
+import styles from './styles.module.scss';
+
+/** Container with top media that doesn't restrict the media's height */
+export default function CategoryThumbnail({ href, imgSrc, title }: Props) {
+  return (
+    <div className={styles.container}>
+      <div className={styles.mediaWrapper}>
+        <InternalLink href={href}>
+          <FluxImage alt={title} src={imgSrc} className={styles.media} />
+        </InternalLink>
+      </div>
+      <Box padding="l" variant="h3">
+        <InternalLink fontSize="heading-s" href={href}>
+          {title}
+        </InternalLink>
+      </Box>
+    </div>
+  );
+}
+
+interface Props {
+  imgSrc: string;
+  href: string;
+  title: string;
+}

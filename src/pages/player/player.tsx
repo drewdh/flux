3f464b36c1eb770d934c4c45800b389df7f2430d
Drew Hanberry <drew.hanberry1@gmail.com>
2025-04-17 20:02:36 -0700

    feat: replace video overlay

--- src/pages/player/player.tsx
+++ src/pages/player/player.tsx
@@ -1,10 +1,29 @@
-import React, { useEffect, useLayoutEffect, useMemo, useRef } from 'react';
+import React, {
+  MouseEventHandler,
+  PropsWithChildren,
+  useEffect,
+  useLayoutEffect,
+  useMemo,
+  useRef,
+  useState,
+} from 'react';
+import Icon, { IconProps } from '@cloudscape-design/components/icon';
+import clsx from 'clsx';
+import { useHover } from 'usehooks-ts';
 
 import styles from './player.module.scss';
-import { topNavSelector } from '../../top-navigation/constants';
 
 export default function Player({ username }: PlayerProps) {
   const player = useRef<any>(null);
+  const [paused, setPaused] = useState<boolean>(false);
+  const [isIdle, setIsIdle] = useState<boolean>(false);
+  const [muted, setMuted] = useState<boolean>(player.current?.getMuted());
+  const overlayRef = useRef<HTMLDivElement>(null);
+  const isOverlayHovered = useHover(overlayRef);
+  const [isInteractiveHovered, setIsInteractiveHovered] = useState<boolean>(false);
+  const idleMs = 3000;
+  const idleTimer = useRef<number>();
+  const overlayVisible = isInteractiveHovered || (isOverlayHovered && !isIdle) || paused;
 
   // Force player to update channel when URL changes
   useEffect(() => {
@@ -18,13 +37,50 @@ export default function Player({ username }: PlayerProps) {
       channel: username,
       autoplay: true,
       muted: false,
+      controls: false,
     }),
     [username]
   );
 
-  // Adjust max width so entire video player is visible when side panel is closed
-  const navHeight = document.querySelector(topNavSelector)?.getBoundingClientRect().height ?? 0;
-  const maxPlayerHeight = window.innerHeight - 24 - navHeight;
+  function Interactive({ children }: PropsWithChildren) {
+    return (
+      <div
+        onMouseEnter={() => setIsInteractiveHovered(true)}
+        onMouseLeave={() => setIsInteractiveHovered(false)}
+      >
+        {children}
+      </div>
+    );
+  }
+
+  function togglePlayback() {
+    const isPaused = player.current?.isPaused();
+    if (isPaused) {
+      player.current?.play();
+    } else {
+      player.current?.pause();
+    }
+    setPaused(!isPaused);
+  }
+
+  function toggleMuted() {
+    setMuted((prev) => {
+      player.current?.setMuted(!prev);
+      return !prev;
+    });
+  }
+
+  const clearIdle = () => {
+    setIsIdle(false);
+    clearTimeout(idleTimer.current);
+  };
+
+  const startIdleTimer = () => {
+    clearTimeout(idleTimer.current);
+    idleTimer.current = window.setTimeout(() => {
+      setIsIdle(true);
+    }, idleMs);
+  };
 
   useLayoutEffect(() => {
     if (!player.current) {
@@ -34,6 +90,11 @@ export default function Player({ username }: PlayerProps) {
     // @ts-ignore
     player.current?.addEventListener(Twitch.Player.PLAY, () => {
       player.current?.setMuted(false);
+      setPaused(false);
+    });
+    // @ts-ignore
+    player.current?.addEventListener(Twitch.Player.PAUSE, () => {
+      setPaused(true);
     });
     // @ts-ignore
     player.current?.addEventListener(Twitch.Player.READY, () => {
@@ -46,10 +107,52 @@ export default function Player({ username }: PlayerProps) {
 
   return (
     <div
-      id="twitch-player"
-      style={{ maxHeight: `${maxPlayerHeight}px` }}
+      ref={overlayRef}
       className={styles.wrapper}
-    />
+      onClick={togglePlayback}
+      onMouseMove={() => {
+        if (!isOverlayHovered) {
+          return;
+        }
+        clearIdle();
+        startIdleTimer();
+      }}
+      onMouseEnter={() => {
+        clearIdle();
+        startIdleTimer();
+      }}
+      onMouseLeave={() => {
+        setIsIdle(false);
+        clearTimeout(idleTimer.current);
+      }}
+    >
+      <div id="twitch-player" className={styles.player} />
+      <div className={clsx(styles.overlay, overlayVisible && styles.visible)} />
+      <div className={clsx(styles.background, overlayVisible && styles.visible)} />
+      <div
+        className={clsx(styles.controls, overlayVisible && styles.visible)}
+        onClick={(e) => e.stopPropagation()}
+      >
+        <Interactive>
+          <IconButton onClick={togglePlayback} iconName={paused ? 'play' : 'pause'} />
+        </Interactive>
+        <Interactive>
+          <IconButton onClick={toggleMuted} iconName={muted ? 'audio-off' : 'audio-full'} />
+        </Interactive>
+      </div>
+    </div>
+  );
+}
+
+interface IconButtonProps {
+  onClick?: MouseEventHandler<HTMLButtonElement>;
+  iconName: IconProps.Name;
+}
+function IconButton({ onClick, iconName }: IconButtonProps) {
+  return (
+    <button onClick={onClick} className={styles.iconButton}>
+      <Icon name={iconName} size="inherit" />
+    </button>
   );
 }
 

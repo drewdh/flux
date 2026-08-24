82f9710bf48bf6eee810e6483e1edee4795df206
Drew Hanberry <drew.hanberry1@gmail.com>
2026-08-24 14:35:19 -0700

    fix: video thumbnail accessibility

--- src/common/video-thumbnail/index.tsx
+++ src/common/video-thumbnail/index.tsx
@@ -1,7 +1,9 @@
+import { useState } from 'react';
 import { colorBackgroundInputDisabled } from '@cloudscape-design/design-tokens';
 import Box from '@cloudscape-design/components/box';
 import SpaceBetween from '@cloudscape-design/components/space-between';
-import { generatePath, Link } from 'react-router-dom';
+import { generatePath, Link, useNavigate } from 'react-router-dom';
+import clsx from 'clsx';
 
 import { Pathname } from 'utilities/routes';
 import Avatar from 'common/avatar';
@@ -10,23 +12,34 @@ import { Stream } from '../../api/twitch-types';
 import FluxImage from 'common/flux-image';
 
 export default function VideoThumbnail({ live, stream, variant = 'normal' }: VideoThumbnailProps) {
+  const navigate = useNavigate();
   const videoHref = generatePath(Pathname.Live, { user: stream.user_login });
   const viewerCount = stream.viewer_count.toLocaleString(undefined, {
     notation: 'compact',
   });
+  const [isFocused, setIsFocused] = useState<boolean>(false);
 
   return (
-    <Link to={videoHref} className={styles.wrapper}>
-      <FluxImage
-        style={{
-          aspectRatio: '16 / 9',
-          backgroundColor: colorBackgroundInputDisabled,
-          width: '100%',
-          display: 'block',
-        }}
-        alt={stream.title}
-        src={`https://static-cdn.jtvnw.net/previews-ttv/live_user_${stream.user_login}-440x248.jpg`}
-      />
+    <div
+      className={clsx(styles.wrapper, isFocused && styles.focused)}
+      onPointerEnter={() => setIsFocused(true)}
+      onPointerLeave={() => setIsFocused(false)}
+      onFocus={() => setIsFocused(true)}
+      onBlur={() => setIsFocused(false)}
+      onClick={() => navigate(videoHref)}
+    >
+      <Link to={videoHref} aria-hidden="true" tabIndex={-1} onClick={(e) => e.stopPropagation()}>
+        <FluxImage
+          style={{
+            aspectRatio: '16 / 9',
+            backgroundColor: colorBackgroundInputDisabled,
+            width: '100%',
+            display: 'block',
+          }}
+          alt=""
+          src={`https://static-cdn.jtvnw.net/previews-ttv/live_user_${stream.user_login}-440x248.jpg`}
+        />
+      </Link>
       <div className={styles.contentWrapper}>
         <div className={styles.streamer}>
           <img
@@ -41,7 +54,13 @@ export default function VideoThumbnail({ live, stream, variant = 'normal' }: Vid
             </div>
           )}
           <div className={styles.content}>
-            <div className={styles.streamTitle}>{stream.title}</div>
+            <Link
+              to={videoHref}
+              className={styles.streamTitle}
+              onClick={(e) => e.stopPropagation()}
+            >
+              {stream.title}
+            </Link>
             {/*<span className={styles.userName}>{stream.user_name}</span>*/}
             <Box variant="span" display="block" color="text-status-inactive">
               {stream.user_name}
@@ -49,7 +68,7 @@ export default function VideoThumbnail({ live, stream, variant = 'normal' }: Vid
             <Box variant="span" display="block" color="text-status-inactive">
               <SpaceBetween size="xxs" direction="horizontal">
                 <span>{stream.game_name}</span>
-                <span>&bull;</span>
+                <span aria-hidden="true">&bull;</span>
                 <span>{viewerCount} viewers</span>
               </SpaceBetween>
             </Box>
@@ -57,7 +76,7 @@ export default function VideoThumbnail({ live, stream, variant = 'normal' }: Vid
         </div>
         <div className={styles.blur} />
       </div>
-    </Link>
+    </div>
   );
 }
 

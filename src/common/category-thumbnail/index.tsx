43b7449cb492e3bc66380c73cdcc4e72e9e6808e
Drew Hanberry <drew.hanberry1@gmail.com>
2026-08-24 14:45:39 -0700

    improve category thumbnail accessibility

--- src/common/category-thumbnail/index.tsx
+++ src/common/category-thumbnail/index.tsx
@@ -1,22 +1,36 @@
-import { Link } from 'react-router-dom';
+import { useState } from 'react';
+import { Link, useNavigate } from 'react-router-dom';
+import clsx from 'clsx';
 
 import FluxImage from 'common/flux-image';
 import styles from './styles.module.scss';
 
 /** Container with top media that doesn't restrict the media's height */
 export default function CategoryThumbnail({ href, imgSrc, title }: Props) {
+  const [isFocused, setIsFocused] = useState<boolean>(false);
+  const navigate = useNavigate();
+
   return (
-    <Link to={href} className={styles.link}>
-      <div className={styles.container}>
-        {/* Leave alt empty since it's the same text as the category title */}
+    <div
+      onFocus={() => setIsFocused(true)}
+      onBlur={() => setIsFocused(false)}
+      onPointerEnter={() => setIsFocused(true)}
+      onPointerLeave={() => setIsFocused(false)}
+      className={clsx(styles.container, isFocused && styles.focused)}
+      onClick={() => navigate(href)}
+    >
+      {/* Leave alt empty since it's the same text as the category title */}
+      <Link to={href} aria-hidden="true" tabIndex={-1} onClick={(e) => e.stopPropagation()}>
         <FluxImage alt="" src={imgSrc} className={styles.media} />
-        <div className={styles.titleWrapper}>
-          <img src={imgSrc} alt="" aria-hidden="true" className={styles.imageBg} />
-          <div className={styles.imageBlur} />
-          <div className={styles.title}>{title}</div>
-        </div>
+      </Link>
+      <div className={styles.titleWrapper}>
+        <img src={imgSrc} alt="" aria-hidden="true" className={styles.imageBg} />
+        <div className={styles.imageBlur} />
+        <Link to={href} className={styles.title} title={title} onClick={(e) => e.stopPropagation()}>
+          {title}
+        </Link>
       </div>
-    </Link>
+    </div>
   );
 }
 
